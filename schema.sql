45bf2791f8b43325f636d49862a57d8290d454e6
Fursov Dmytro <101358517+Sanctified23@users.noreply.github.com>
2023-05-17 20:23:41 +0300

    Task7 + Task6 (#7)
    * added layout for index.php
    
    * added dynamic calculation of date in badge
    
    * added dynamic calculation of date in badge-2
    
    * fixed issue with echo + layout structure
    
    * "
    
    * added function.php
    
    * added created database sql
    
    * added request for database refresh

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE `projects_and_work`;
+
+CREATE TABLE `projects_and_work`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(255) NOT NULL ,
+    `email` VARCHAR(255) NOT NULL UNIQUE ,
+    `password` VARCHAR(255) NOT NULL ,
+     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `projects_and_work`.`projects`(
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `title` VARCHAR(255) NOT NULL ,
+    `user_id` INT NOT NULL ,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (`id`),
+    INDEX(`user_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `projects_and_work`.`work`
+(
+    `id`          INT          NOT NULL AUTO_INCREMENT,
+    `title`       VARCHAR(255) NOT NULL,
+    `status`      VARCHAR(255) NOT NULL DEFAULT 'backlog',
+    `description` TEXT NULL,
+    `file`        VARCHAR(255) NULL,
+    `user_id`     INT          NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES users (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `project_id`  INT          DEFAULT 1,
+    `deadline`    DATE NULL,
+    FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    INDEX(`status`)
+) ENGINE = InnoDB;
